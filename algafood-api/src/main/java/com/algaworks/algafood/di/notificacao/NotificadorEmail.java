c00337f9955bdb2a036b707a8e6a74ec53b68b00
Marco Jardim <santarelle@gmail.com>
2019-11-23 10:16:25 -0400

    Adicionado System.out.println para verificar como o spring instancia as depencendias

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.di.modelo.Cliente;
 
 @Component
-public class NotificacaoEmail {
+public class NotificadorEmail implements Notificador {
 	
+	public NotificadorEmail() {
+		System.out.println("NotificadorEmail: " + this);
+	}
+	
+	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s atraves do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,13 +3,18 @@ package com.algaworks.algafood.di.service;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NotificacaoEmail;
+import com.algaworks.algafood.di.notificacao.Notificador;
 
 @Component
-public class AvaliacaoClienteService {
+public class AtivacaoClienteService {
 
-	private NotificacaoEmail notificador;
+	private Notificador notificador;
 	
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+		System.out.println("AtivacaoClienteService: " + notificador);
+	}
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
