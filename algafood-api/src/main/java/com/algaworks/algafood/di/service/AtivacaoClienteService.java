2af87c343ea4e1b563c6dd7c736720da8b12b509
Marco Jardim <santarelle@gmail.com>
2019-11-24 13:38:33 -0400

    clean

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,37 +1,16 @@
 package com.algaworks.algafood.di.service;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
 @Component
-public class AtivacaoClienteService implements InitializingBean, DisposableBean {
-
-	@TipoDoNotificador(NivelUrgencia.NORMAL)
-	@Autowired
-	private Notificador notificador;
+public class AtivacaoClienteService {
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro no sistema esta ativo");
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		System.out.println("INIT");
-
-	}
-
-	@Override
-	public void destroy() throws Exception {
-		System.out.println("DESTROY");
+		// emitir evento para o container dizendo que o cliente esta ativo
 	}
 
 }
