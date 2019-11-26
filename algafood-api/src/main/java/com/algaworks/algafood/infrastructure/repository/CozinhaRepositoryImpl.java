c73843fbc904d1f0344b9116229d6b3dd9b4ef27
Marco Jardim <santarelle@gmail.com>
2019-11-26 17:24:25 -0400

    Conhecendo e implementando o padrão Repository

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.jpa;
+package com.algaworks.algafood.infrastructure.repository;
 
 import java.util.List;
 
@@ -7,33 +7,36 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
-import org.springframework.stereotype.Component;
-
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Component
-public class CadastroCozinha {
-
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+	
 	@PersistenceContext
 	private EntityManager manager;
-	
+
+	@Override
 	public List<Cozinha> listar() {
 		TypedQuery<Cozinha> query = manager.createQuery("FROM Cozinha", Cozinha.class);
 		return query.getResultList();
 	}
 	
 	@Transactional
+	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
 	
+	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
 	@Transactional
+	@Override
 	public void remover(Cozinha cozinha) {
 		cozinha = buscar(cozinha.getId());
 		manager.remove(cozinha);
 	}
+
 }
