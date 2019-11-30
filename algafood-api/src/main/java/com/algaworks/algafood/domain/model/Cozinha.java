d19fe19aae2c0122a16a09310d2da5dfafd46241
Marco Jardim <santarelle@gmail.com>
2019-11-29 22:06:33 -0400

    clean

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,14 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@JsonRootName("gastronomia")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
