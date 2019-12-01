26009e854e6e52788630860ead2f65892abe02d2
Marco Jardim <santarelle@gmail.com>
2019-12-01 15:35:54 -0400

    Modelando e implementando a inclusão de recursos de restaurantes

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Nao existe cadastro de cozinha com codigo %d", cozinhaId));
+		}
+		return restauranteRepository.salvar(restaurante);
+	}
+}
