b92b1a5b4f39433fc6ee85199ac33cc3f32f37e9
Marco Jardim <santarelle@gmail.com>
2019-12-17 19:21:29 -0400

    findByNome

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,6 +1,9 @@
 
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,6 @@ import com.algaworks.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-//	List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findTodasByNome(String nome);
+	Optional<Cozinha> findByNome(String nome);
 }
