20f181e1fa75d44f5fc573e590ee49465d5f44a2
Marco Jardim <santarelle@gmail.com>
2020-01-19 14:50:45 -0400

    Implementando uma consulta simples com Criteria API

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -23,29 +23,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		
-		var jpql = new StringBuilder();
-		jpql.append("FROM Restaurante WHERE 0 = 0 ");
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		var parametros = new HashMap<String, Object>();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);
 		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("AND nome LIKE :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
-		
-		if (taxaFreteInicial != null) {
-			jpql.append("AND taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-		
-		if (taxaFreteFinal != null) {
-			jpql.append("AND taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);	
-		}
-		
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		
 		return query.getResultList();
 	}
