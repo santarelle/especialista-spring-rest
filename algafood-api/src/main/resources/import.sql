4b9fbabad60c9c8f31a7e9287b5a102e8dd21f32
Marco Jardim <santarelle@gmail.com>
2020-01-21 19:52:50 -0400

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -10,3 +10,14 @@ INSERT INTO estado (nome) VALUES ('Sao Paulo');
 
 INSERT INTO cidade (nome, estado_id) VALUES ('Manaus', 1);
 INSERT INTO cidade (nome, estado_id) VALUES ('Campinas', 2);
+
+INSERT INTO forma_pagamento (id, descricao) VALUES (1, 'Cartao de crédito');
+INSERT INTO forma_pagamento (id, descricao) VALUES (2, 'Cartao de débito');
+INSERT INTO forma_pagamento (id, descricao) VALUES (3, 'Dinheiro');
+
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES (1, 1);
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES (1, 2);
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES (1, 3);
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES (2, 3);
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES (3, 2);
+INSERT INTO restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) VALUES (3, 3);
