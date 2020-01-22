c7d7538f4929bc1f9395464ccb6ffad7dca56128
Marco Jardim <santarelle@gmail.com>
2020-01-22 19:59:17 -0400

    Embeddable

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -69,7 +69,7 @@ public class RestauranteController {
 				return ResponseEntity.notFound().build();
 			}
 
-			BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formaPagamentos");
+			BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formaPagamentos", "endereco");
 			Restaurante restauranteSalvo = cadastroRestaurante.salvar(restauranteAtual.get());
 			return ResponseEntity.ok(restauranteSalvo);
 
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,15 +1,15 @@
 INSERT INTO cozinha (id, nome) VALUES (1, 'Tailandesa');
 INSERT INTO cozinha (id, nome) VALUES (2, 'Indiana');
 
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Bela Mineira', 10, 1);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Picanha Mania', 20, 1);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Bobs', 20, 2);
-
 INSERT INTO estado (nome) VALUES ('Amazonas');
 INSERT INTO estado (nome) VALUES ('Sao Paulo');
 
 INSERT INTO cidade (nome, estado_id) VALUES ('Manaus', 1);
 INSERT INTO cidade (nome, estado_id) VALUES ('Campinas', 2);
+
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_complemento, endereco_bairro) VALUES ('Bela Mineira', 10, 1, 1, '69033-050', 'Rua Prof Clotilde', null, 'Sao Jorge');
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Picanha Mania', 20, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Bobs', 20, 2);
 
 INSERT INTO forma_pagamento (id, descricao) VALUES (1, 'Cartao de crédito');
 INSERT INTO forma_pagamento (id, descricao) VALUES (2, 'Cartao de débito');
