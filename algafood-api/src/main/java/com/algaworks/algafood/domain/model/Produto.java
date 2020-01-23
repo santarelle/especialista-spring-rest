e6afd48fb42a757503d9605399fbcd6853fba9c8
Marco Jardim <santarelle@gmail.com>
2020-01-22 21:25:07 -0400

    Desafio: mapeando relacionamento muitos-para-um

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Produto {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String contentType;
+	private String tamanho;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+
+}
