73d0cfc23d2d232678ce136457cb3853ec47bd87
Marco Jardim <santarelle@gmail.com>
2020-07-15 22:18:27 -0400

    Add Constrains validation on cascade

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -34,6 +35,8 @@ public class Restaurante {
     private BigDecimal taxaFrete;
 
     //	@JsonIgnore
+    @Valid
+    @NotNull
     @ManyToOne // (fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
