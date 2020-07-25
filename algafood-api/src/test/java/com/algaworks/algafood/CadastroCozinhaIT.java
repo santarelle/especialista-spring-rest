704d44dcbbb4ca4f90f42fa7c17b6c725fb5f0ba
Marco Jardim <santarelle@gmail.com>
2020-07-25 14:18:54 -0400

    Refactor CadastroCozinhaIT.java

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafood;
 
 import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import com.algaworks.algafood.util.DatabaseCleaner;
+import com.algaworks.algafood.util.ResourceUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
@@ -29,6 +29,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 
+    private static final int COZINHA_ID_NONEXISTENT = 0;
+
     @LocalServerPort
     private int port;
 
@@ -41,11 +43,17 @@ public class CadastroCozinhaIT {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    private int quantityCozinhasRegistered;
+    private Cozinha cozinhaIndiana;
+    private String jsonCozinhaChinesa;
+
     @Before
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();;
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        jsonCozinhaChinesa = ResourceUtils.getContentFromResource("/json/cozinha-chinesa.json");
 
         databaseCleaner.clearTables();
         prepareDatabase();
@@ -71,7 +79,7 @@ public class CadastroCozinhaIT {
 
     @Test(expected = CozinhaNaoEncontradaException.class)
     public void shouldFail_WhenDeleteCozinhaNonexistent() {
-        cadastroCozinhaService.excluir(0L);
+        cadastroCozinhaService.excluir((long) COZINHA_ID_NONEXISTENT);
     }
 
     @Test
@@ -85,20 +93,19 @@ public class CadastroCozinhaIT {
     }
 
     @Test
-    public void shouldHave2Cozinhas_WhenFindCozinhas() {
+    public void shouldReturnQuantityCozinhasRegisteredCorrectly_WhenFindCozinhas() {
         given()
                 .accept(ContentType.JSON)
         .when()
             .get()
         .then()
-            .body("", Matchers.hasSize(2))
-            .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+            .body("", Matchers.hasSize(quantityCozinhasRegistered));
     }
 
     @Test
     public void shouldReturnStatusCREATED_WhenCreateCozinha() {
         given()
-            .body("{ \"nome\": \"Polonesa\" }")
+            .body(jsonCozinhaChinesa)
             .contentType(ContentType.JSON)
             .accept(ContentType.JSON)
         .when()
@@ -107,13 +114,37 @@ public class CadastroCozinhaIT {
             .statusCode(HttpStatus.CREATED.value());
     }
 
+    @Test
+    public void shouldResponseStatusOKAndNomeIndiana_WhenFindCozinhaWithId2() {
+        given()
+            .pathParam("cozinhaId", cozinhaIndiana.getId())
+            .accept(ContentType.JSON)
+        .when()
+            .get("/{cozinhaId}")
+        .then()
+            .statusCode(HttpStatus.OK.value())
+            .body("nome", Matchers.equalTo(cozinhaIndiana.getNome()));
+    }
+
+    @Test
+    public void shouldResponseStatusNotFound_WhenFindCozinhaNonexistent() {
+        given()
+            .pathParam("cozinhaId", COZINHA_ID_NONEXISTENT)
+        .when()
+            .get("/{cozinhaId}")
+        .then()
+            .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
     private void prepareDatabase() {
         Cozinha cozinha1 = new Cozinha();
         cozinha1.setNome("Tailandesa");
         cozinhaRepository.save(cozinha1);
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Indiana");
-        cozinhaRepository.save(cozinha2);
+        cozinhaIndiana = new Cozinha();
+        cozinhaIndiana.setNome("Indiana");
+        cozinhaRepository.save(cozinhaIndiana);
+
+        quantityCozinhasRegistered = (int) cozinhaRepository.count();
     }
 }
