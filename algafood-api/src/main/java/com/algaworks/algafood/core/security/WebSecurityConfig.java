b69bd74eb0e03a3b34f862cf0cd1a57cec20dbb5
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2021-04-12 11:24:20 -0400

    Configure in memory auth users

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,5 +23,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var password = new BCryptPasswordEncoder().encode("123");
+        auth.inMemoryAuthentication()
+                    .withUser("marco")
+                    .password(password)
+                    .roles("ADMIN")
+                .and()
+                    .withUser("maria")
+                    .password(password)
+                    .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
