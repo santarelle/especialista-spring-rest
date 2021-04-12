5cee378744d208f643da8f58f390523ccbd73034
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2021-04-12 14:01:40 -0400

    Configure Authorization Server

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
@@ -1,7 +1,9 @@
-package com.algaworks.algafood.core.security;
+package com.algaworks.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,8 +26,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var password = new BCryptPasswordEncoder().encode("123");
+        auth.inMemoryAuthentication()
+                .withUser("marco")
+                .password(password)
+                .roles("ADMIN")
+                .and()
+                .withUser("maria")
+                .password(password)
+                .roles("ADMIN");
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
