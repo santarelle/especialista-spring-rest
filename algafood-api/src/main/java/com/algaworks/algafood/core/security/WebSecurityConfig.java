9e74a5bbf0ac86875143c2851258152659ebf87f
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2021-04-12 11:16:01 -0400

    Enable basic authorization

---
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable();
+    }
+}
