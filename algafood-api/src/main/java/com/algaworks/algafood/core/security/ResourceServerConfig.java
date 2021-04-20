9fcb5305306f4ea9e9680715c828473236bd512c
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2021-04-19 21:10:01 -0400

    Configure CORS on authorization server and api

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -16,6 +16,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .cors().and()
                 .oauth2ResourceServer().opaqueToken();
     }
 }
